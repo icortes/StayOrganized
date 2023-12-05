52aee0c77480f28eaed6965d902d0b50b9d8a4e6
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-12-05 14:34:28 -0800

    fixed date format in post

--- scripts/new_todo.js
+++ scripts/new_todo.js
@@ -38,7 +38,7 @@ function makeNewTodo() {
       const userid = document.getElementById('names').value;
       const category = document.getElementById('categories').value;
       const description = document.getElementById('task').value.trim();
-      const deadline = document.getElementById('deadline').value.trim();
+      const deadline = document.getElementById('deadline').value.trim().replace(/-/g, '/');
       const priority = document.getElementById('priority').value;
       let response = await fetch('http://localhost:8083/api/todos', {
         method: 'POST',
