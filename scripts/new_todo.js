88819103b28714880d9ef1b1bc4f1aa295f06001
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-12-05 14:15:43 -0800

    new todo post feature functional

--- scripts/new_todo.js
+++ scripts/new_todo.js
@@ -31,7 +31,30 @@ async function addCategoriesToDrop() {
   }
 }
 
+function makeNewTodo() {
+  document.getElementById('newTaskForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    try {
+      const userid = document.getElementById('names').value;
+      const category = document.getElementById('categories').value;
+      const description = document.getElementById('task').value.trim();
+      const deadline = document.getElementById('deadline').value.trim();
+      const priority = document.getElementById('priority').value;
+      let response = await fetch('http://localhost:8083/api/todos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userid, category, description, deadline, priority }),
+      });
+      let data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
 onload = () => {
   addUsersToDrop();
   addCategoriesToDrop();
+  makeNewTodo();
 };
